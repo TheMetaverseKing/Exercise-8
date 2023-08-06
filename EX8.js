3a7ad4fac506f524033052b0ab4acf35fa05073c
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-06 11:09:33 +0500

    Add files via upload

--- EX8.js
+++ EX8.js
@@ -1,5 +1,10 @@
 "use strict";
+console.log("-".repeat(90));
 console.log(1 + 7);
+console.log("_".repeat(90));
 console.log(0 + 8);
+console.log(">".repeat(90));
 console.log(2 + 6);
+console.log(".".repeat(90));
 console.log(4 + 4);
+console.log("=".repeat(90));
--- EX8.ts
+++ EX8.ts
@@ -1,4 +1,11 @@
+
+
+console.log("-".repeat(90))
 console.log(1+7);
+console.log("_".repeat(90))
 console.log(0+8);
+console.log(">".repeat(90))
 console.log(2+6);
+console.log(".".repeat(90))
 console.log(4+4);
+console.log("=".repeat(90))
